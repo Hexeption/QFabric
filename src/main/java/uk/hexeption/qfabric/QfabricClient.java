434e605d3f596d7554943d4801097e6a204da97d
Hexeption <minecraftfun201@gmail.com>
2020-04-27 19:17:02 +0100

    Initial commit

---
+++ src/main/java/uk/hexeption/qfabric/QfabricClient.java
@@ -0,0 +1,13 @@
+package uk.hexeption.qfabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class QfabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/uk/hexeption/qfabric/QfabricMain.java
@@ -0,0 +1,10 @@
+package uk.hexeption.qfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class QfabricMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
